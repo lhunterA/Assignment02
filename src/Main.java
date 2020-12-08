d7b4fc06dd2d288d1bfcea160ceec16fdabde79e
lhunterA <200425671@student.georgianc.on.ca>
2020-12-08 17:33:01 -0500

    Made the Main, started creating files and packages

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 }
+
+// https://pokeapi.co/api/v2/pokemon/{id or name}/
